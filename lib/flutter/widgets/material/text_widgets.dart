9173b1c393594d6a379ac2bee6049f9f307a96d3
apon <mdaponahmed412@gmail.com>
2024-05-03 10:36:03 +0600

    SelectableText add

--- lib/flutter/widgets/material/text_widgets.dart
+++ lib/flutter/widgets/material/text_widgets.dart
@@ -5,29 +5,35 @@ class TextWidgets extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: Center(
-        child: Text(
-          'Text Hello',
-          textAlign: TextAlign.start,
-          overflow: TextOverflow.fade,
-          maxLines: 1,
-          style: TextStyle(
-            color: Colors.red,
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            wordSpacing: 4,
-            shadows: [
-              Shadow(
-                color: Colors.blue,
-                blurRadius: 1,
-                offset: Offset(5, 5),
-              ),
-            ],
+        child: Column(
+          children: [
+            const Text(
+              'Text Hello',
+              textAlign: TextAlign.start,
+              overflow: TextOverflow.fade,
+              maxLines: 1,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                wordSpacing: 4,
+                shadows: [
+                  Shadow(
+                    color: Colors.blue,
+                    blurRadius: 1,
+                    offset: Offset(5, 5),
+                  ),
+                ],
 
-            /// many more
-          ),
+                /// many more
+              ),
+            ),
+            const SelectableText('SelectableText'),
+            Text('Hi '*300),
+          ],
         ),
       ),
     );
