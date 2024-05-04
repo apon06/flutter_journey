f675c1464e94633e768e2831dbbef74b8c6f3387
apon <mdaponahmed412@gmail.com>
2024-05-04 16:26:14 +0600

    enum

---
+++ lib/flutter/Learn the Basics of Dart/enum.dart
@@ -0,0 +1,23 @@
+enum AppColors { red, blue, yellow, green }
+
+// ignore: constant_identifier_names
+enum Weekday { Monday, Tuesday, Wednesday, Thursday, Friday }
+
+void main() {
+  AppColors baseColors = AppColors.yellow;
+
+  if (baseColors == AppColors.blue) {
+    print('Yes');
+  } else {
+    print('No');
+  }
+  print(baseColors.index); // 2
+
+  Weekday today = Weekday.Friday;
+
+  ///split make this arrey and value.remove.value and last function selected last value the code
+  print("Today is ${today.toString().split('.').last}");
+
+  /// make arrey ebery value.Output : [W, e, e, k, d, a, y, ., F, r, i, d, a, y]
+  print(today.toString().split(''));
+}
