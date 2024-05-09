06eba792075059968562ffdeffc62338f5538bb8
apon <mdaponahmed412@gmail.com>
2024-05-09 19:29:41 +0600

    flutterlogo

---
+++ lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/material/flutterlogo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FlutterLogoWidgets extends StatelessWidget {
+  const FlutterLogoWidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
