27a0f6db0f6d285f0a8d6cd02f2709abdb08d218
apon <mdaponahmed412@gmail.com>
2024-05-10 16:48:20 +0600

    SharedPreferencesLocalStoreInt

---
+++ lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/local Storage/shared_preferences/shared_preferences_int.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesLocalStoreInt extends StatefulWidget {
+  const SharedPreferencesLocalStoreInt({super.key});
+
+  @override
+  State<SharedPreferencesLocalStoreInt> createState() =>
+      SsharedPreferencesStateLocalStore();
+}
+
+class SsharedPreferencesStateLocalStore
+    extends State<SharedPreferencesLocalStoreInt> {
+  int count = 0;
+
+  savaData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    int? saveValue = sharedPreferences.getInt('Int');
+    setState(() {
+      if (saveValue != null) {
+        count = saveValue;
+      } 
+    });
+  }
+
+  add() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {
+      count++;
+      sharedPreferences.setInt('Int', count);
+    });
+  }
+
+  @override
+  void initState() {
+    savaData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          add();
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text(count.toString()),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_journey/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/web%20sockets/web_sockets.dart';
-import 'package:web_socket_channel/io.dart';
+import 'package:flutter_journey/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/local%20Storage/shared_preferences/shared_preferences_int.dart';
 import 'flutter/1_Learn_the_Basics_of_Dart/2_widgets/inherited/inherited_widgets.dart';
 
 void main() {
@@ -12,13 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MyInheritedWidgets(
+    return const MyInheritedWidgets(
       age: 18,
       child: MaterialApp(
         title: 'Flutter Learning',
-        home: WebSocketTest(
-          channel: IOWebSocketChannel.connect('ws://echo.websocket.org'),
-        ),
+        home: SharedPreferencesLocalStoreInt(),
       ),
     );
   }
