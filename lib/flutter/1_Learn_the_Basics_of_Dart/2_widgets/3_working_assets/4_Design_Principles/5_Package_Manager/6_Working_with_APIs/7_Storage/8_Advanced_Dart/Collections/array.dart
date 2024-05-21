158b498dc246ba1dcaad8fe8c9d05660694ec1b4
apon <mdaponahmed412@gmail.com>
2024-05-21 20:56:40 +0600

    array

---
+++ lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/8_Advanced_Dart/Collections/array.dart
@@ -0,0 +1,10 @@
+void main() {
+  var array = [4, true, 3.1416, 'pi'];
+  array.remove(4);
+  print(array);
+  array.add(5);
+  print(array);
+  print(array.length);
+  // array.clear();
+  // print(array); //! []
+}
