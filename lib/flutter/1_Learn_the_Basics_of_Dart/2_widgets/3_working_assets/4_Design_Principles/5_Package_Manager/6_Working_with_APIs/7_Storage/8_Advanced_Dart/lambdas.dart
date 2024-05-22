9a70dc14e087f42de6a4976977f1655b0306fbeb
apon <mdaponahmed412@gmail.com>
2024-05-22 07:41:14 +0600

    return add

--- lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/8_Advanced_Dart/lambdas.dart
+++ lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/8_Advanced_Dart/lambdas.dart
@@ -1,11 +1,15 @@
 void main() {
- var myName =  (String name){
+  var myName = (String name) {
     print('Name = $name');
   };
 
- Function myRoll =  (int roll){
+  Function myRoll = (int roll) {
     print('Roll = $roll');
+  };
+  var cube = (int cube) {
+    return cube * cube * cube;
   };
   myName('Apon');
   myRoll(105);
+  print(cube(3));
 }
