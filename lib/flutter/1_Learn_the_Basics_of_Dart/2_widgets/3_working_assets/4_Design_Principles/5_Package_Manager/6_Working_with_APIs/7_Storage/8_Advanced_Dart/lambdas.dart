327b3a0b3a36ba805ac0af87e4c118e66acb87d3
apon <mdaponahmed412@gmail.com>
2024-05-22 07:33:15 +0600

    lambdas function

---
+++ lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/8_Advanced_Dart/lambdas.dart
@@ -0,0 +1,11 @@
+void main() {
+ var myName =  (String name){
+    print('Name = $name');
+  };
+
+ Function myRoll =  (int roll){
+    print('Roll = $roll');
+  };
+  myName('Apon');
+  myRoll(105);
+}
