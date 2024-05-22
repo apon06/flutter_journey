1447de31ce08825e263a6e4cd83a1c6dc62b36cc
apon <mdaponahmed412@gmail.com>
2024-05-22 07:23:57 +0600

    print remove

--- lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/8_Advanced_Dart/Collections/array.dart
+++ lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/7_Storage/8_Advanced_Dart/Collections/array.dart
@@ -1,10 +1,10 @@
 void main() {
   var array = [4, true, 3.1416, 'pi'];
   array.remove(4);
-  print(array);
+  // print(array);
   array.add(5);
-  print(array);
-  print(array.length);
+  // print(array);
+  // print(array.length);
   // array.clear();
   // print(array); //! []
 }
