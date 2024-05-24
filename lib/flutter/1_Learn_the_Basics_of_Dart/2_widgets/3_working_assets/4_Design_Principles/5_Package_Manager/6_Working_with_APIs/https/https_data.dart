7db81759e413b13f3ce94372bff43db42a9ad601
apon <mdaponahmed412@gmail.com>
2024-05-24 11:20:54 +0600

    https data store shared_preferences package

--- lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/https/https_data.dart
+++ lib/flutter/1_Learn_the_Basics_of_Dart/2_widgets/3_working_assets/4_Design_Principles/5_Package_Manager/6_Working_with_APIs/https/https_data.dart
@@ -1,6 +1,60 @@
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+
+// class HttpsInternetData extends StatefulWidget {
+//   const HttpsInternetData({super.key});
+
+//   @override
+//   State<HttpsInternetData> createState() => HttpsInternetDataState();
+// }
+
+// class HttpsInternetDataState extends State<HttpsInternetData> {
+//   dynamic data;
+//   Future getData() async {
+//     var res =
+//         await http.get(Uri.parse('https://aponali.github.io/api/allapon.json'));
+//     setState(() {
+//       var decode = json.decode(res.body);
+//       data = decode;
+//       print(data);
+//     });
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     getData();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: ListView.builder(
+//         itemCount: data == null ? 0 : data.length,
+//         itemBuilder: (BuildContext context, int index) {
+//           return Text(data[index]["allappname"]);
+//         },
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
 import 'dart:convert';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HttpsInternetData extends StatefulWidget {
   const HttpsInternetData({super.key});
@@ -10,20 +64,34 @@ class HttpsInternetData extends StatefulWidget {
 }
 
 class HttpsInternetDataState extends State<HttpsInternetData> {
-  dynamic data;
-  Future getData() async {
-    var res =
-        await http.get(Uri.parse('https://aponali.github.io/api/allapon.json'));
-    setState(() {
-      var decode = json.decode(res.body);
-      data = decode;
-      print(data);
-    });
+  List<dynamic>? data;
+
+  Future<void> saveData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    List<String>? saveValue = sharedPreferences.getStringList('Int');
+    if (saveValue != null) {
+      setState(() {
+        data = saveValue.map((e) => json.decode(e)).toList();
+      });
+    }
+  }
+
+  Future<void> getData() async {
+    var res = await http.get(Uri.parse('https://aponali.github.io/api/allapon.json'));
+    if (res.statusCode == 200) {
+      var decode = json.decode(res.body) as List;
+      setState(() {
+        data = decode;
+      });
+      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+      sharedPreferences.setStringList('Int', decode.map((e) => json.encode(e)).toList());
+    }
   }
 
   @override
   void initState() {
     super.initState();
+    saveData();
     getData();
   }
 
@@ -31,9 +99,15 @@ class HttpsInternetDataState extends State<HttpsInternetData> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: data == null ? 0 : data.length,
+        itemCount: data == null ? 0 : data!.length,
         itemBuilder: (BuildContext context, int index) {
-          return Text(data[index]["allappname"]);
+          return SizedBox(
+            height: 100,
+            width: 70,
+            child: CachedNetworkImage(
+              imageUrl: data![index]["allappimage"],
+            ),
+          );
         },
       ),
     );
